668e5c25d58aff70c888cd8eca04f3792fc6a06d
Harrison Ravazzolo <38767391+harrisonravazzolo@users.noreply.github.com>
2026-01-08 17:04:48 -0800

    clean up

--- rdp.ps1
+++
@@ -1,7 +0,0 @@
-# Enable Remote Desktop
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
-
-# Allow RDP through Windows Firewall
-New-NetFirewallRule -DisplayName "RDP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 3389 -Enabled True
-
-Write-Output "Remote Desktop enabled. Please restart your computer."
